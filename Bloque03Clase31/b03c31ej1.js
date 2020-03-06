30bd00235bb1f7a57c4cd160521f99fe18f66240
Jorge <jurgenblumen@gmail.com>
2020-03-05 21:06:09 -0600

    Actualizado con la clase 35 y cambios en clase 10,31

--- Bloque03Clase31/b03c31ej1.js
+++ Bloque03Clase31/b03c31ej1.js
@@ -10,6 +10,65 @@
 
 //tu código va aquí vvv
 
+const avengers = [
+    {
+        "nombre": "Iron Man",
+        "identidad": "Tony Stark",
+        "superpoder": "Super Traje",
+        "primerpelicula": "Iron Man 1"
+    },
+    {
+        "nombre": "Capitan America",
+        "identidad": "Steve Rogers",
+        "superpoder": "Super Soldado",
+        "primerpelicula": "Capitan America 1"
+    },
+    {
+        "nombre": "Thor",
+        "identidad": "Thor Odinson",
+        "superpoder": "Poder del Rayo",
+        "primerpelicula": "Thor 1"
+    },
+    {
+        "nombre": "Black Widow",
+        "identidad": "Natasha Romanov",
+        "superpoder": "Combate",
+        "primerpelicula": "Avengers 1"
+    },
+    {
+        "nombre": "Hulk",
+        "identidad": "Bruce Banner",
+        "superpoder": "Fuerza Gamma",
+        "primerpelicula": "Hulk 1"
+    }
+];
+
+const mitabla = document.createElement("table");
+const headerRow = document.createElement("th");
+
+
+
+let row, col, txt;
+const tableRows = [];
+for (let i = 0; i < avengers.length; i++) {
+
+    row = document.createElement("tr");
+    tableRows.push(row);
+
+    for (let prop in avengers[i]) {
+
+        col = document.createElement("td");
+        txt = document.createTextNode(avengers[i][prop]);
+        col.appendChild(txt);
+        tableRows[i].appendChild(col);
+    }
+}
+for (let tablerow of tableRows) {
+    mitabla.appendChild(tablerow);
+}
+
+    const el_body = document.getElementById("body");
+el_body.appendChild(mitabla);
 //tu código va aquí ^^^
 
 //eof
---
+++ Bloque03Clase35/b03c35ej1.js
@@ -0,0 +1,22 @@
+/*
+    Este ejercicio debe correr en Node.
+
+    Este ejercicio es un reto complicado.
+    No te desesperes y pide ayuda si la necesitas.
+
+    Crea un arreglo que tenga sólo números enteros.
+    Debe ser un arreglo grande (al menos 10 elementos).
+
+    Ordena el arreglo de menor a mayor.
+
+    Conoce el álgoritmo de búsqueda binaria:
+    https://youtu.be/m2-e2O7wvbk
+
+    Implementa este algoritmo de forma recursiva.
+*/
+
+//tu código va aquí vvv
+
+//tu código va aquí ^^^
+
+//eof
