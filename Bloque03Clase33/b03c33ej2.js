ec376b96783547e59153395d6b68d5d6bfa6aca0
Jorge <jurgenblumen@gmail.com>
2020-03-14 18:05:31 -0600

    Ejercicios clase 33 terminados

--- Bloque03Clase33/b03c33ej2.js
+++ Bloque03Clase33/b03c33ej2.js
@@ -11,6 +11,32 @@
 
 //tu código va aquí vvv
 
+const arr1 = [4, 8, 15, 16, 23];
+const arr2 = [9, 17, 92, 43, 12];
+const arr3 = [9, 8, 19, 92, 27];
+const arr4 = [3, 14, 16, 19, 18];
+
+function cinco(a, b, c, d, e) {
+    console.log("Recibí parámetros: " + a + ", " + b + ", " + c + ", " + d + ", " + e + ".")
+}
+cinco(...arr1);
+cinco(...arr2);
+cinco(...arr3);
+cinco(...arr4);
+
+function muchos(...chingo) {
+    let str = "Recibí parámetros: ";
+    for (let i of chingo) {
+        str += i + ", ";
+    }
+    console.log(str);
+}
+muchos(1, 2, 3, 4, 5, 6, 6);
+muchos(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+muchos("lol", 256, undefined, true);
+muchos();
+muchos(...arr4);
+
 //tu código va aquí ^^^
 
 //eof
--- Bloque03Clase33/b03c33ej3.js
+++ Bloque03Clase33/b03c33ej3.js
@@ -1,18 +1,36 @@
 /*
     Este ejercicio debe correr en Node.
 
-    Crea un objeto cualquiera, y agrégale algunos parámetros.
+    Crea un objeto cualquiera, y agrégale algunas propiedades.
     Agrégale una arrow function guardada en una de sus propiedades.
     Esa arrow function debe imprimir otra de las propiedades del objeto.
 
     Crea la misma arrow function fuera del objeto, pero que reciba un objeto como argumento.
 
     Llama ambas funciones y comprueba lo que sucede.
-    ¿Puedes explicar el fenómeno en términos de this?    
+    ¿Puedes explicar el fenómeno en términos de this? Tiene el this global, no esta definido tal cual y por ello es undefined
 */
 
 //tu código va aquí vvv
+const obj = {
+    "nombre"    : "Pepe",
+    "caracter"  : "Enojona",
+    "edad"      : "6",
+    "sexo"      : "es 2020 no asumas mi genero",
+    "estatura"  : "desconocida",
+    "poder"     : "electrico",
+    "frase"     : "ooolovoooorgooo",
+    "funcion"   : () => {
+        console.log (this.sexo);
+    }
+};
 
+const funcion = (o) => {
+    console.log (o.sexo);
+};
+
+obj.funcion();
+funcion({});
 //tu código va aquí ^^^
 
 //eof
