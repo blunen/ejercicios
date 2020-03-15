d0bc33efa0845bc909b9449b1588e2c77c5e0087
Jorge <jurgenblumen@gmail.com>
2020-03-14 18:49:26 -0600

    Clase 33 ej1 resuelto

--- Bloque03Clase33/b03c33ej1.js
+++ Bloque03Clase33/b03c33ej1.js
@@ -10,6 +10,32 @@
 
 //tu código va aquí vvv
 
+const PRIMERSYMBOL = Symbol("1");
+const SEGUNDOSYMBOL = Symbol("2");
+
+const obj = {
+    "name" : "Jose",
+    "apellido" : "Jose", 
+    [PRIMERSYMBOL] : "el principe",
+    [SEGUNDOSYMBOL] : "de la cancion"
+}
+
+function printProtoChain(obj, ind="") {
+    ind2 = ind+"    ";
+    if(obj==null) {
+        console.log("\n"+ind+"null");
+        return;
+    }
+    console.log("\n"+ind+obj.constructor.name+" : {\n");
+    for(let p of Object.getOwnPropertySymbols(obj)) {
+        console.log(p, obj[p]);
+    }
+    printProtoChain(Object.getPrototypeOf(obj), ind2);
+    console.log(ind+"}"); //cerrar bloque de objeto
+}
+
+printProtoChain(obj);
+
 //tu código va aquí ^^^
 
 //eof
